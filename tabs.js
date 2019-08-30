9003609a8d02df33c956ea7448f8f54e59100777
Blake Gilbertson <blakegilbertson@protonmail.com>
2019-08-30 10:12:29 +0100

    Adding JS to handle the tab click events and show/hide of tab content

--- tabs.js
+++ tabs.js
@@ -1,1 +1,28 @@
-console.log('log from tabs.js');
+
+// function to remove the selected status
+const removeSelectedClass = elClass => {
+  const el = document.getElementsByClassName(elClass)
+  for(let i = 0; i < el.length; i++) {
+    el[i].classList.remove('selected')
+  }
+}
+
+// function to handle the click event
+const handleTabClick = (e) => {
+  const clickedTabId = e.target.id
+  const clickedTabNum = clickedTabId.split('tab')[1]
+  const pageToSelect = `page${clickedTabNum}`
+
+  removeSelectedClass('tab')
+  removeSelectedClass('page')
+
+  document.getElementById(pageToSelect).classList.add('selected')
+}
+
+// Adding event lisenter to tabs
+const selectedTab = document.getElementsByClassName('tab')
+
+for(let i = 0; i < selectedTab.length; i++) {
+  selectedTab[i].addEventListener('click', handleTabClick, false)
+}
+
